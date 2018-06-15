aab01d13a2f96e7c5366e6e3433ab6d1b428fc4c
thiwakar.velusamy <thiwakar.velusamy@BDC4-L-CFBYGZ1>
2018-06-15 17:33:23 +0530

    Add project files.

---
+++ VSTSGitDemo/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace VSTSGitDemo
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
